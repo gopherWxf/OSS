deeb1d65d0e98db33fa6b376f1263ff3553501f7
gopherwxf <68725032@qq.com>
2022-11-19 20:54:24 +0800

    删除对象

--- apiServer/objects/del.go
+++ apiServer/objects/del.go
@@ -5,32 +5,37 @@ import (
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
-	"strconv"
 	"strings"
 )
 
 func Del(ctx *gin.Context) {
 	r := ctx.Request
 	w := ctx.Writer
-
 	defer r.Body.Close()
-	//获取object的名称
-	object := strings.Split(r.URL.EscapedPath(), "/")[2]
+
+	// 获得桶名
+	bucket := strings.Split(r.URL.EscapedPath(), "/")[2]
+	if bucket == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	// 获取对象名称
+	name := strings.Split(r.URL.EscapedPath(), "/")[3]
+
 	//从es中获取object的最新版本
-	version, err := es.SearchLatestVersion(object)
+	version, err := es.SearchLatestVersion(bucket, name)
 	if err != nil {
 		log.Println(err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	if version.Version == "" {
-		log.Println("Not found", object)
+	if version.Version == 0 {
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
+
 	//向es中插入object一个版本，size=0,hash=""，代表着是插入标记
-	v, _ := strconv.Atoi(version.Version)
-	err = es.PutMetadata(object, v+1, 0, "")
+	err = es.PutMetadata(bucket, name, version.Version+1, 0, "")
 	if err != nil {
 		log.Println(err)
 		w.WriteHeader(http.StatusInternalServerError)
