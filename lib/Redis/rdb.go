c94fd9a215f5b6b889828c53e8d8407868b3ce02
gopherwxf <68725032@qq.com>
2022-11-19 23:04:26 +0800

    debug

--- lib/Redis/rdb.go
+++ lib/Redis/rdb.go
@@ -4,44 +4,42 @@ import (
 	"context"
 	"github.com/go-redis/redis/v8"
 	"log"
-	"os"
-	"strings"
 )
 
 type RDB struct {
-	//Client *redis.Client
-	Client *redis.ClusterClient
+	Client *redis.Client
+	//Client *redis.ClusterClient
+}
+
+func NewRedis(redisAddr string) *RDB {
+	client := new(RDB)
+	client.Client = redis.NewClient(&redis.Options{
+		Addr: "101.43.17.240:6379",
+
+		//Addr:     redisAddr,
+		//Username: "root",
+		//Password: "123456",
+	})
+	return client
 }
 
 //func NewRedis(redisAddr string) *RDB {
 //	client := new(RDB)
-//	client.Client = redis.NewClient(&redis.Options{
-//		Addr: "101.43.17.240:6379",
 //
-//		//Addr:     redisAddr,
-//		//Username: "root",
-//		//Password: "123456",
+//	redisClusterAddrsString := os.Getenv("REDIS_CLUSTER")
+//	redisClusterAddr := strings.Split(redisClusterAddrsString, ",")
+//
+//	client.Client = redis.NewClusterClient(&redis.ClusterOptions{
+//		Addrs:    redisClusterAddr,
+//		Password: os.Getenv("REDIS_PASSWORD"),
+//		PoolSize: 20,
 //	})
+//	_, err := client.Client.Ping(context.Background()).Result()
+//	if err != nil {
+//		panic(err)
+//	}
 //	return client
 //}
-
-func NewRedis(redisAddr string) *RDB {
-	client := new(RDB)
-
-	redisClusterAddrsString := os.Getenv("REDIS_CLUSTER")
-	redisClusterAddr := strings.Split(redisClusterAddrsString, ",")
-
-	client.Client = redis.NewClusterClient(&redis.ClusterOptions{
-		Addrs: redisClusterAddr,
-		//Password: os.Getenv("REDIS_PASSWORD"),
-		PoolSize: 20,
-	})
-	_, err := client.Client.Ping(context.Background()).Result()
-	if err != nil {
-		panic(err)
-	}
-	return client
-}
 
 func (rdb *RDB) Publish(channel string, message interface{}) {
 	err := rdb.Client.Publish(context.Background(), channel, message).Err()
